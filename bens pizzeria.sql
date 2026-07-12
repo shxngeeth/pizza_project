3ae0a9d3540f67cf82d0d589d959d4b60a01a745
SHANGEETH VANNIYASINGAM <144239557+shxngeeth@users.noreply.github.com>
2026-07-12 23:38:41 +0100

    Add files via upload

---
+++ bens pizzeria.sql
@@ -0,0 +1,15 @@
+SELECT 
+o.order_id,
+i.item_id,
+o.quantity,
+i.item_cat,
+i.item_name,
+o.created_at,
+a.delivery_address1,
+a.delivery_address2,
+a.delivery_city,
+a.delivery_zipcode,
+o.delivery
+FROM
+orders o
+LEFT JOIN item i ON
